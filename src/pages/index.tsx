7acd829f8ce2b3ff351bac147eb6cd0e128b4ff0
João <joao.rocha@atonit.com.br>
2023-04-23 22:38:04 -0300

    adding strapi to home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,19 +12,22 @@ import { STRAPI_API_URL, STRAPI_URL, restAPI } from '../rest/env';
 
 // @ts-ignore: next-line
 export async function getStaticProps({ locale }) {
-  const products = await restAPI('strapi', 'products');
-  const categories = await restAPI('strapi', 'categories');
+  const categoriesShowcase = await restAPI('strapi', 'homepage/categories');
+  const productsShowcase = await restAPI('strapi', 'homepage/products');
+  const cardsShowcase = await restAPI('strapi', 'homepage/cards');
+
   return {
     props: {
-      categories: categories.data,
-      products: products.data,
+      categoriesShowcase: categoriesShowcase.data.attributes.categoriesShowcase,
+      productsShowcase: productsShowcase.data.attributes.productsShowcase,
+      cardsShowcase: cardsShowcase.data.attributes.cardsShowcase,
       ...await serverSideTranslations(locale, ['components', 'common'])
     },
   }
 }
 
 // @ts-ignore: next-line
-export default function Home({products, categories}) {
+export default function Home({categoriesShowcase, productsShowcase, cardsShowcase}) {
   return (
     <>
       <Head>
@@ -35,58 +38,67 @@ export default function Home({products, categories}) {
         <Minicart/>
         <div className='container'>
           <section className='section--categoryNavigate'>
-            <h2>Navegue por categorias</h2>
+            <h2>{categoriesShowcase.title}</h2>
             <ListNavigate>
-              <Tile href={'/category/'+categories[0].id} image={{src: STRAPI_URL+categories[0].attributes.thumb.data.attributes.formats.thumbnail.url, alt: categories[0].attributes.thumb.data.attributes.alternativeText}} text={categories[0].attributes.name}/>
-              <Tile href={'/category/'+categories[1].id} image={{src: STRAPI_URL+categories[1].attributes.thumb.data.attributes.formats.thumbnail.url, alt: categories[1].attributes.thumb.data.attributes.alternativeText}} text={categories[1].attributes.name}/>
-              <Tile href={'/category/'+categories[2].id} image={{src: STRAPI_URL+categories[2].attributes.thumb.data.attributes.formats.thumbnail.url, alt: categories[2].attributes.thumb.data.attributes.alternativeText}} text={categories[2].attributes.name}/>
-              <Tile href={'/category/'+categories[3].id} image={{src: STRAPI_URL+categories[3].attributes.thumb.data.attributes.formats.thumbnail.url, alt: categories[3].attributes.thumb.data.attributes.alternativeText}} text={categories[3].attributes.name}/>
+              {
+                (categoriesShowcase.categories && categoriesShowcase.categories.data.length > 0)
+                ?
+                  categoriesShowcase.categories.data.map((category: any, index: any) => (
+                    <Tile href={'/category/'+category.id} image={{src: STRAPI_URL+category.attributes.thumb.data.attributes.formats.thumbnail.url, alt: category.attributes.thumb.data.attributes.alternativeText}} text={category.attributes.name} key={`categories-home-${categoriesShowcase.title}-${category.id}`}/>
+                  ))
+                :
+                  null
+              }
             </ListNavigate>
           </section>
-          <section className='section--productNavigate'>
-            <h2>Mais vendidos</h2>
-            <ListNavigate>
-              <ProductTile pid={products[0].id} productObject={products[0].attributes}/>
-              <ProductTile pid={products[1].id} productObject={products[1].attributes}/>
-              <ProductTile pid={products[2].id} productObject={products[2].attributes}/>
-            </ListNavigate>
-          </section>
-          <section className='section--productNavigate'>
-            <h2>Recomendados</h2>
-            <ListNavigate>
-              <ProductTile pid={products[0].id} productObject={products[0].attributes}/>
-              <ProductTile pid={products[1].id} productObject={products[1].attributes}/>
-              <ProductTile pid={products[2].id} productObject={products[2].attributes}/>
-            </ListNavigate>
-          </section>
-          <Card>
-              <TileSquare href={'/category/1'} image={{src:'/images/category/smartphone.webp', alt: 'A smartphone with so much colors in background'}} text="Smartphones"/>
-          </Card>
-          <Card>
-            <div className='grid--card'>
-              <TileSquare href={'/category/'+categories[0].id} image={{src: STRAPI_URL+categories[0].attributes.thumb.data.attributes.formats.thumbnail.url, alt: categories[0].attributes.thumb.data.attributes.alternativeText}} text={categories[0].attributes.name}/>
-              <TileSquare href={'/category/'+categories[1].id} image={{src: STRAPI_URL+categories[1].attributes.thumb.data.attributes.formats.thumbnail.url, alt: categories[1].attributes.thumb.data.attributes.alternativeText}} text={categories[1].attributes.name}/>
-              <TileSquare href={'/category/'+categories[2].id} image={{src: STRAPI_URL+categories[2].attributes.thumb.data.attributes.formats.thumbnail.url, alt: categories[2].attributes.thumb.data.attributes.alternativeText}} text={categories[2].attributes.name}/>
-              <TileSquare href={'/category/'+categories[3].id} image={{src: STRAPI_URL+categories[3].attributes.thumb.data.attributes.formats.thumbnail.url, alt: categories[3].attributes.thumb.data.attributes.alternativeText}} text={categories[3].attributes.name}/>
-            </div>
-          </Card>
-          <Card>
-            <TileSquare href={'/category/1'} image={{src:'/images/category/smartphone.webp', alt: 'A smartphone with so much colors in background'}} text="Smartphones"/>
-          </Card>
-          <Card>
-            <TileSquare href={'/category/1'} image={{src:'/images/category/smartphone.webp', alt: 'A smartphone with so much colors in background'}} text="Smartphones"/>
-          </Card>
-
-          <Card>
-            <div className='grid--card'>
-              <TileSquare href={'/category/'+categories[1].id} image={{src: STRAPI_URL+categories[1].attributes.thumb.data.attributes.formats.thumbnail.url, alt: categories[1].attributes.thumb.data.attributes.alternativeText}} text={categories[1].attributes.name}/>
-              <TileSquare href={'/category/'+categories[1].id} image={{src: STRAPI_URL+categories[1].attributes.thumb.data.attributes.formats.thumbnail.url, alt: categories[1].attributes.thumb.data.attributes.alternativeText}} text={categories[1].attributes.name}/>
-              <TileSquare href={'/category/'+categories[1].id} image={{src: STRAPI_URL+categories[1].attributes.thumb.data.attributes.formats.thumbnail.url, alt: categories[1].attributes.thumb.data.attributes.alternativeText}} text={categories[1].attributes.name}/>
-              <TileSquare href={'/category/'+categories[1].id} image={{src: STRAPI_URL+categories[1].attributes.thumb.data.attributes.formats.thumbnail.url, alt: categories[1].attributes.thumb.data.attributes.alternativeText}} text={categories[1].attributes.name}/>
-              <TileSquare href={'/category/'+categories[1].id} image={{src: STRAPI_URL+categories[1].attributes.thumb.data.attributes.formats.thumbnail.url, alt: categories[1].attributes.thumb.data.attributes.alternativeText}} text={categories[1].attributes.name}/>
-              <TileSquare href={'/category/'+categories[1].id} image={{src: STRAPI_URL+categories[1].attributes.thumb.data.attributes.formats.thumbnail.url, alt: categories[1].attributes.thumb.data.attributes.alternativeText}} text={categories[1].attributes.name}/>
-            </div>
-          </Card>
+          {
+            (productsShowcase && productsShowcase.length > 0)
+            ?
+            productsShowcase.map((productShowcase : any, index : Number) => (
+                <section className='section--productNavigate' key={`productNavigate-${index}`}>
+                  <h2>{productShowcase.title}</h2>
+                  <ListNavigate>
+                    {
+                      (productShowcase.products && productShowcase.products.data.length > 0)
+                      ?
+                        productShowcase.products.data.map((product: any, index: any) => (
+                          <ProductTile pid={product.id} productObject={product.attributes} key={`productItem-${productShowcase.title}-${index}`}/>
+                        ))
+                      :
+                        null
+                    }
+                  </ListNavigate>
+                </section>
+              ))
+            :
+              null
+          }
+          {
+            (cardsShowcase && cardsShowcase.length > 0)
+            ?
+              cardsShowcase.map((cardShowcase: any, index: Number) => (
+                <Card title={cardShowcase.title} key={`cardsShowcase-home-${cardShowcase.id}`}>
+                  {
+                    (cardShowcase.cards)?
+                      (cardShowcase.cards.length > 1)
+                      ?
+                        <div className='grid--card'>
+                          {
+                            cardShowcase.cards.map((card : any, index : Number) => (
+                              <TileSquare href={card.redirect} image={{src: STRAPI_URL+card.thumb.data.attributes.url, alt: card.thumb.data.attributes.alternativeText}} text={card.title} key={`cardShowcase-card-${cardShowcase.id}-${card.id}-${index}`}/>
+                            ))
+                          }
+                        </div>
+                      :
+                        <TileSquare href={cardShowcase.cards[0].redirect} image={{src: STRAPI_URL+cardShowcase.cards[0].thumb.data.attributes.url, alt: cardShowcase.cards[0].thumb.data.attributes.alternativeText}} text={cardShowcase.cards[0].title}/>
+                    :
+                      null
+                  }
+                </Card>
+              ))
+            :
+              null
+          }
         </div>
       </main>
       <Footer/>
